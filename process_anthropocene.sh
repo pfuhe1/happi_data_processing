ed1b4f94bbc0404f784bcb683c9d2d5a4f88d0c7
Peter Uhe <Peter.Uhe@bristol.ac.uk>
2020-08-25 10:26:22 +0100

    added absolute precip multi-model summary script

--- process_anthropocene.sh
+++ process_anthropocene.sh
@@ -9,12 +9,13 @@
 #python indices_portaldata_clim_v4.py CanESM2
 
 # Loop over indices and call scripts for this index and MIP
-for index in RXx5day #RXx1day pryrmean
+for index in RXx5day RXx1day pryrmean
 do
   # Calculate region averages for AR6 regions (script loops over models)
   python IPCC_AR6regs_calc_indices.py $index
 
   # Calculate summary statistics from vales of indices for IPCC AR6 regions
   python IPCC_AR6regs_summary_multimodel_indices.py $index #(default is HAPPI models only)
+  python IPCC_AR6regs_absprecipsummary_multimodel_indices.py $index #(default is HAPPI models only)
 
 done
